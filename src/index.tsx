c478d7ae9d1e0c7c1d69f133338a5a80e5141d9a
Valeria <valleriademelo@gmail.com>
2024-10-17 01:23:22 -0300

    Update package.json with browserslist and add .gitignore file

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+  return <h1>Welcome to Binance Websocket Pricing App</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
